f755f486958c0704e49ff6715a7ad600aa43bbd6
Ezra <gerkibz@outlook.com>
2022-04-06 20:30:21 +0300

    agrs whats my name function update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,9 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
 	return (0);
 }
 // int main(int argc, char *argv[])
