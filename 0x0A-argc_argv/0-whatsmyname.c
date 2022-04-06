bc48b27b2eb4ed5bf5eb557048e4bbe828fe5e22
Ezra <gerkibz@outlook.com>
2022-04-06 20:31:11 +0300

    agrs whats my name function update 2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,17 +7,11 @@
  * @argv: argument value, a strings that come after calling function
  * Return: Always 0 (Success)
  */
-int main(int argc __attribute__((unused)), char *argv[])
+
+int main(int argc, char *argv[])
 {
 	if (argc > 0)
 		printf("%s\n", argv[0]);
 
 	return (0);
 }
-// int main(int argc, char *argv[])
-// {
-// 	if (argc > 0)
-// 		printf("%s", argv[0]);
-
-// 	return (0);
-// }
