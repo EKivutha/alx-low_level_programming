43cb619be7f852bafd02315192d116fea899980e
Ezra <gerkibz@outlook.com>
2022-04-07 23:16:37 +0300

    update string length checker

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,14 +17,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 
-	while (s1[lens1])
-		lens1++;
-
+	// while (s1[lens1])
+	// 	lens1++;
+	lens1 = _strlen(s1)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s2[lens2])
-		lens2++;
+	// while (s2[lens2])
+	// 	lens2++;
+	lens2 = _strlen(s2)
 
 	if (n >= lens2)
 		n = lens2;
