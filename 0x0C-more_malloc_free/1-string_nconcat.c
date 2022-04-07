61d6ded3cce1f3e63bd043d93198171dfefabd39
Ezra <gerkibz@outlook.com>
2022-04-07 22:55:30 +0300

    changed NULL to string for malloc address space size

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	new_str = malloc(lens1 + n + 1);
 	if (new_str == NULL)
-		return (NULL);
+		return ("");
 
 	for (; i < (lens1 + n); i++)
 	{
